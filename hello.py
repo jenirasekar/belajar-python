a8898abd8cc69e54da75930f8a4f9b6a67656421
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-07 20:44:15 +0800

    condition

--- hello.py
+++ hello.py
@@ -1,9 +1,27 @@
 # tugas bang dea
 
-saldo_awal = input('berapa saldo awalmu: ')
-deposit = input('mau deposit berapa? ')
+# saldo_awal = input('berapa saldo awalmu: ')
+# deposit = input('mau deposit berapa? ')
+#
+# # munculkan output ke terminal hasil setelah saldo awal ditambahkan dengan deposit
+#
+# saldo_akhir = int(saldo_awal) + int(deposit)
+# print('saldo akhir: ' + str(saldo_akhir))
 
-# munculkan output ke terminal hasil setelah saldo awal ditambahkan dengan deposit
+# pekondisian
+usia = int(input('kamu umur berapa? '))
 
-saldo_akhir = int(saldo_awal) + int(deposit)
-print('saldo akhir: ' + str(saldo_akhir))
+if usia >= 0 and usia <= 2:
+    print('batita')
+elif usia >= 3 and usia <= 4:
+    print('balita')
+elif usia >= 5 and usia <= 12:
+    print('anak-anak')
+elif usia >= 13 and usia <= 18:
+    print('remaja')
+elif usia >= 18 and usia <= 30:
+    print('dewasa')
+elif usia >= 30 and usia <= 60:
+    print('orang tua')
+else:
+    print('bau tanah jir')
