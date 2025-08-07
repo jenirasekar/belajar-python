360f38f5ab94216737fb95853f2edd6da331a6a4
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-07 20:13:37 +0800

    data type conversion

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-# print("Hello, world!")
-x = "anjay"
-def myFunc():
-    global x
-    x = "hello"
-myFunc()
-print(x)
+# tugas bang dea
+
+saldo_awal = input('berapa saldo awalmu: ')
+deposit = input('mau deposit berapa? ')
+
+# munculkan output ke terminal hasil setelah saldo awal ditambahkan dengan deposit
+
+saldo_akhir = int(saldo_awal) + int(deposit)
+print('saldo akhir: ' + str(saldo_akhir))
