e4af0eee7c57f55278da4ada4f8a44894b49f119
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-17 16:07:42 +0800

    insert items

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 # append items
-fruits = ["apple", " orange", "banana"]
-fruits.append("orange")
-print(fruits)
+# fruits = ["apple", " orange", "banana"]
+# fruits.append("orange")
+# print(fruits)
+
+# insert items
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(0,"orange")
+# print(fruits)
