4da13c0d555bac087c03c9ff683a03cfaaea4b4e
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-17 15:42:00 +0800

    lists: change list items

---
+++ main.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+fruits[3:6] = ["blackcurrant, 'durian", "pamelo"]
+print(fruits)
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
