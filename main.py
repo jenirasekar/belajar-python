e775f9d1dad13a7d292633b7777532fd8dc4f7d4
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-17 15:44:27 +0800

    append items

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
-fruits[3:6] = ["blackcurrant, 'durian", "pamelo"]
+# append items
+fruits = ["apple", " orange", "banana"]
+fruits.append("orange")
 print(fruits)
-
-thislist = ["apple", "banana", "cherry"]
-thislist[1:3] = ["watermelon"]
-print(thislist)
