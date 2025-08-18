12b42997679e14a90d472382c77b39866c40ec30
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-18 23:55:09 +0800

    remove list

--- main.py
+++ main.py
@@ -7,3 +7,16 @@
 # fruits = ["apple", "banana", "cherry"]
 # fruits.insert(0,"orange")
 # print(fruits)
+
+# list comprehension
+fruits = ["apple", "banana", "cherry"]
+new_fruits = []
+for x in fruits:
+    if "a" in x:
+        new_fruits.append(x)
+        print(new_fruits)
+
+# newlist = [expression for item in iterable if condition == True]
+new_fruits = [x for x in fruits if "a" in x]
+new_fruits.remove("apple")
+print(new_fruits)
