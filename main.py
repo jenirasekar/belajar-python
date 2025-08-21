6c914d7277072ceea0777b2a09b0343dd49b87ea
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2025-08-21 17:19:16 +0800

    sort list alphanumerically

--- main.py
+++ main.py
@@ -9,15 +9,19 @@
 # print(fruits)
 
 # list comprehension
-fruits = ["apple", "banana", "cherry"]
-new_fruits = []
-for x in fruits:
-    if "a" in x:
-        new_fruits.append(x)
-        print(new_fruits)
+# fruits = ["apple", "banana", "cherry"]
+# new_fruits = []
+# for x in fruits:
+#     if "a" in x:
+#         new_fruits.append(x)
+#         print(new_fruits)
+#
+# # newlist = [expression for item in iterable if condition == True]
+# new_fruits = [x for x in fruits if "a" in x]
+# new_fruits.append("durian")
+# print(new_fruits)
 
-# newlist = [expression for item in iterable if condition == True]
-new_fruits = [x for x in fruits if "a" in x]
-new_fruits.remove("apple")
-new_fruits.append("durian")
-print(new_fruits)
+# sort list alphanumerically
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
